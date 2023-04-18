eff7133474ddbf6eaa34537019d8645c691d2509
LedZeppe1in <tualatin32@mail.ru>
2023-04-18 16:11:30 +0800

    Updated configs

--- config/web.php
+++ config/web.php
@@ -4,27 +4,63 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => 'alexandernikolaychuk',
+    'name' => 'Alexander Nikolaychuk',
+    'defaultRoute' => 'main/default/index',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+
+    // all site modules
+    'modules' => [
+        'main' => [
+            'class' => 'app\modules\main\Module',
+        ],
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+            'as access' => [
+                'class' => 'yii\filters\AccessControl',
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ]
+                ]
+            ],
+        ]
+    ],
+
     'components' => [
+        'language' => 'ru-RU',
         'request' => [
+            'class' => 'app\components\LangRequest',
+            // site root directory
+            'baseUrl' => '',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'avBgNUAB4sj6Y0YjpGoOcsQDlXLxnVtq',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'class' => 'app\components\LangUrlManager',
+            'rules' => [
+                'index' => 'main/default/index',
+                'sing-in' => 'main/default/sing-in'
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
+            'identityClass' => 'app\modules\admin\models\User',
             'enableAutoLogin' => true,
+            'loginUrl' => ['main/default/sing-in'],
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => 'main/default/error',
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
@@ -42,14 +78,20 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
+        'i18n' => [
+            'translations' => [
+                'app' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages',
+                    'forceTranslation' => true,
+                    'sourceLanguage' => 'en-US',
+                ],
             ],
         ],
-        */
+        'formatter' => [
+            'datetimeFormat' => 'dd.MM.Y HH:mm',
+            'timeZone' => 'UTC',
+        ],
     ],
     'params' => $params,
 ];

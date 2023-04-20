c9bfa25bcbf2cacb021ec3c73b5cbb314f5b4009
LedZeppe1in <tualatin32@mail.ru>
2023-04-20 09:05:42 +0800

    Added main RU and EN messages

---
+++ messages/en/app.php
@@ -0,0 +1,154 @@
+<?php
+
+return [
+    /* Пункты главного меню клиентской части сайта */
+    'NAV_HOME' => 'Home',
+    'NAV_BIOGRAPHY' => 'Biography',
+    'NAV_CONCERTS' => 'Concerts',
+    'NAV_MEDIA' => 'Media',
+    'NAV_MUSIC' => 'Music',
+    'NAV_PROJECTS' => 'Projects',
+    'NAV_REPERTOIRE' => 'Repertoire',
+    'NAV_CONTACTS' => 'Contacts',
+    'NAV_SIGN_IN' => 'Sign in',
+    /* Пункты главного меню административной части сайта */
+    'NAV_ADMIN_ACCOUNT' => 'Account',
+    'NAV_ADMIN_SIGNED_IN_AS' => 'Signed in as',
+    'NAV_ADMIN_MY_PROFILE' => 'My profile',
+    'NAV_ADMIN_MY_BIOGRAPHY' => 'My biography',
+    'NAV_ADMIN_SIGN_OUT' => 'Sign out',
+    'NAV_ADMIN_CONCERTS' => 'Concerts',
+    'NAV_ADMIN_MEDIA' => 'Media',
+    'NAV_ADMIN_MUSIC' => 'Music',
+    'NAV_ADMIN_PROJECTS' => 'Projects',
+    'NAV_ADMIN_REPERTOIRE' => 'Repertoire',
+
+    /* Пункты правого меню */
+    'SIDE_NAV_POSSIBLE_ACTIONS' => 'Possible actions',
+
+    /* Нижний колонтитул (подвал) */
+    'FOOTER_POWERED_BY' => 'Powered by',
+    'FOOTER_DEVELOPER' => 'Nikita Dorodnykh',
+
+    /* Общие кнопки */
+    'BUTTON_ADD' => 'Add',
+    'BUTTON_SAVE' => 'Save',
+    'BUTTON_SIGN_IN' => 'Sign in',
+    'BUTTON_CREATE' => 'Create',
+    'BUTTON_UPDATE' => 'Update',
+    'BUTTON_DELETE' => 'Delete',
+    'BUTTON_CANCEL' => 'Cancel',
+
+    /* Общие сообщения об ошибках */
+    'ERROR_MESSAGE_PAGE_NOT_FOUND' => 'Page not found.',
+    'ERROR_MESSAGE_ACCESS_DENIED' => 'You are not allowed to perform this action.',
+
+    /* Страница ошибки */
+    'ERROR_PAGE_NOTICE' => 'The above error occurred while the Web server was processing your request.',
+    'ERROR_PAGE_TEXT' => 'Please contact us if you think this is a server error. Thank you.',
+
+    /* Страницы клиентской части сайта */
+    /* Страница концертов */
+    'CONCERTS_PAGE_TITLE' => 'Concerts',
+    /* Страница медиа */
+    'MEDIA_PAGE_TITLE' => 'Media',
+    /* Страница музыки */
+    'MUSIC_PAGE_TITLE' => 'Music',
+    /* Страница проектов */
+    'PROJECTS_PAGE_TITLE' => 'Projects',
+    /* Страница репертуара */
+    'REPERTOIRE_PAGE_TITLE' => 'Repertoire',
+
+    /* Страница контактов */
+    'CONTACTS_PAGE_TITLE' => 'Contacts',
+    'CONTACTS_PAGE_TOUCH_WITH_ME' => 'Get in touch with me',
+    'CONTACTS_PAGE_NOTICE' => 'If you have business inquiries or other questions, please fill out the following form to contact me. Thank you!',
+    'CONTACTS_PAGE_SUCCESS_MESSAGE' => 'Thank you for contacting us. We will respond to you as soon as possible.',
+    /* Страница входа */
+    'SIGN_IN_PAGE_TITLE' => 'Sign in',
+    'SIGN_IN_PAGE_TEXT' => 'Please fill out the following fields to sign in:',
+    'SIGN_IN_PAGE_RESET_TEXT' => 'If you forgot your password you can',
+    'SIGN_IN_PAGE_RESET_LINK' => 'reset it',
+
+    /* Страницы административной части сайта */
+    /* Страницы учетной записи пользователя (администратора) */
+    'USER_ADMIN_PAGE_MY_PROFILE' => 'My profile',
+    'USER_ADMIN_PAGE_MY_BIOGRAPHY' => 'My biography',
+    'USER_ADMIN_PAGE_UPDATE_DATA' => 'Update account data',
+    'USER_ADMIN_PAGE_UPDATE_BIOGRAPHY' => 'Update biography',
+    'USER_ADMIN_PAGE_UPDATE_PASSWORD' => 'Update password',
+    /* Кнопки на страницах учетной записи пользователя (администратора) */
+    'USER_ADMIN_PAGE_BUTTON_UPDATE_DATA' => 'Update account data',
+    'USER_ADMIN_PAGE_BUTTON_UPDATE_BIOGRAPHY' => 'Update biography',
+    'USER_ADMIN_PAGE_BUTTON_UPDATE_PASSWORD' => 'Update password',
+    /* Сообщения на страницах аучетной записи пользователя (администратора) */
+    'USER_ADMIN_PAGE_MESSAGE_UPDATED_DATA' => 'You have successfully changed account data.',
+    'USER_ADMIN_PAGE_MESSAGE_UPDATED_BIOGRAPHY' => 'You have successfully changed biography.',
+    'USER_ADMIN_PAGE_MESSAGE_UPDATE_PASSWORD' => 'You have successfully changed password.',
+    /* Страницы администрирования концертов */
+    'CONCERTS_ADMIN_PAGE_CONCERTS' => 'Concerts',
+    /* Страницы администрирования медиа */
+    'MEDIA_ADMIN_PAGE_MEDIA' => 'Media',
+    /* Страницы администрирования музыки */
+    'MUSIC_ADMIN_PAGE_MUSIC' => 'Music',
+    /* Страницы администрирования проектов */
+    'PROJECTS_ADMIN_PAGE_PROJECTS' => 'Projects',
+    /* Страницы администрирования проектов */
+    'REPERTOIRE_ADMIN_PAGE_REPERTOIRE' => 'Repertoire',
+
+    /* Формы */
+    /* ContactForm */
+    'CONTACT_FORM_NAME' => 'Name',
+    'CONTACT_FORM_EMAIL' => 'E-mail',
+    'CONTACT_FORM_SUBJECT' => 'Subject',
+    'CONTACT_FORM_MESSAGE' => 'Message',
+    'CONTACT_FORM_VERIFICATION_CODE' => 'Verification Code',
+    /* LoginForm */
+    'LOGIN_FORM_USERNAME' => 'Username',
+    'LOGIN_FORM_PASSWORD' => 'Password',
+    'LOGIN_FORM_REMEMBER_ME' => 'Remember Me',
+    /* Сообщения LoginForm */
+    'LOGIN_FORM_MESSAGE_INCORRECT_USERNAME_OR_PASSWORD' => 'Username or password is incorrect.',
+
+    /* Модели */
+    /* Lang */
+    'LANG_MODEL_ID' => 'ID',
+    'LANG_MODEL_CREATED_AT' => 'Created at',
+    'LANG_MODEL_UPDATED_AT' => 'Updated at',
+    'LANG_MODEL_URL' => 'URL',
+    'LANG_MODEL_LOCAL' => 'Local',
+    'LANG_MODEL_NAME' => 'Name',
+    'LANG_MODEL_DEFAULT' => 'Default language',
+    /* User */
+    'USER_MODEL_ID' => 'ID',
+    'USER_MODEL_CREATED_AT' => 'Created at',
+    'USER_MODEL_UPDATED_AT' => 'Updated at',
+    'USER_MODEL_USERNAME' => 'Username',
+    'USER_MODEL_PASSWORD' => 'Password',
+    'USER_MODEL_AUTH_KEY' => 'Auth key',
+    'USER_MODEL_EMAIL_CONFIRM_TOKEN' => 'E-mail confirm token',
+    'USER_MODEL_PASSWORD_HASH' => 'Password hash',
+    'USER_MODEL_PASSWORD_RESET_TOKEN' => 'Password reset token',
+    'USER_MODEL_FULL_NAME_RU' => 'Full name in Russian',
+    'USER_MODEL_FULL_NAME_EN' => 'Full name in English',
+    'USER_MODEL_ADDRESS_RU' => 'Address in Russian',
+    'USER_MODEL_ADDRESS_EN' => 'Address in English',
+    'USER_MODEL_BIOGRAPHY_RU' => 'Biography in Russian',
+    'USER_MODEL_BIOGRAPHY_EN' => 'Biography in English',
+    'USER_MODEL_EMAIL' => 'E-mail',
+    'USER_MODEL_PHONE' => 'Phone',
+    'USER_MODEL_YOUTUBE_LINK' => 'YouTube link',
+    'USER_MODEL_INSTAGRAM_LINK' => 'Instagram link',
+    'USER_MODEL_FACEBOOK_LINK' => 'Facebook link',
+    'USER_MODEL_TWITTER_LINK' => 'Twitter link',
+    'USER_MODEL_VK_LINK' => 'VK link',
+    'USER_MODEL_APPLE_MUSIC_LINK' => 'Apple Music link',
+    'USER_MODEL_YANDEX_MUSIC_LINK' => 'Yandex Music link',
+    'USER_MODEL_SPOTIFY_LINK' => 'Spotify link',
+    /* Сообщения модели User */
+    'USER_MODEL_MESSAGE_USERNAME' => 'This username has already been taken.',
+    'USER_MODEL_MESSAGE_UPDATED_YOUR_DETAILS' => 'You have successfully changed your details.',
+    'USER_MODEL_MESSAGE_UPDATED_YOUR_PASSWORD' => 'You have successfully changed password.',
+    'USER_MODEL_MESSAGE_FULL_NAME_RU' => 'Your full name must be in Russian.',
+    'USER_MODEL_MESSAGE_FULL_NAME_EN' => 'Your full name must be in English.',
+];
---
+++ messages/ru/app.php
@@ -0,0 +1,154 @@
+<?php
+
+return [
+    /* Пункты главного меню клиентской части сайта */
+    'NAV_HOME' => 'Главная',
+    'NAV_BIOGRAPHY' => 'Биография',
+    'NAV_CONCERTS' => 'Концерты',
+    'NAV_MEDIA' => 'Медиа',
+    'NAV_MUSIC' => 'Музыка',
+    'NAV_PROJECTS' => 'Проекты',
+    'NAV_REPERTOIRE' => 'Репертуар',
+    'NAV_CONTACTS' => 'Контакты',
+    'NAV_SIGN_IN' => 'Вход',
+    /* Пункты главного меню административной части сайта */
+    'NAV_ADMIN_ACCOUNT' => 'Учётная запись',
+    'NAV_ADMIN_SIGNED_IN_AS' => 'Вы вошли как',
+    'NAV_ADMIN_MY_PROFILE' => 'Мой профиль',
+    'NAV_ADMIN_MY_BIOGRAPHY' => 'Моя биография',
+    'NAV_ADMIN_SIGN_OUT' => 'Выход',
+    'NAV_ADMIN_CONCERTS' => 'Концерты',
+    'NAV_ADMIN_MEDIA' => 'Медиа',
+    'NAV_ADMIN_MUSIC' => 'Музыка',
+    'NAV_ADMIN_PROJECTS' => 'Проекты',
+    'NAV_ADMIN_REPERTOIRE' => 'Репертуар',
+
+    /* Пункты правого меню */
+    'SIDE_NAV_POSSIBLE_ACTIONS' => 'Возможные действия',
+
+    /* Нижний колонтитул (подвал) */
+    'FOOTER_POWERED_BY' => 'Разработано',
+    'FOOTER_DEVELOPER' => 'Никита Дородных',
+
+    /* Общие кнопки */
+    'BUTTON_ADD' => 'Добавить',
+    'BUTTON_SAVE' => 'Сохранить',
+    'BUTTON_SIGN_IN' => 'Войти',
+    'BUTTON_CREATE' => 'Создать',
+    'BUTTON_UPDATE' => 'Обновить',
+    'BUTTON_DELETE' => 'Удалить',
+    'BUTTON_CANCEL' => 'Отмена',
+
+    /* Общие сообщения об ошибках */
+    'ERROR_MESSAGE_PAGE_NOT_FOUND' => 'Страница не найдена.',
+    'ERROR_MESSAGE_ACCESS_DENIED' => 'Вам не разрешено производить данное действие.',
+
+    /* Страница ошибки */
+    'ERROR_PAGE_NOTICE' => 'Вышеупомянутая ошибка произошла при обработке веб-сервером вашего запроса.',
+    'ERROR_PAGE_TEXT' => 'Пожалуйста, свяжитесь с нами, если Вы думаете, что это ошибка сервера. Спасибо.',
+
+    /* Страницы клиентской части сайта */
+    /* Страница концертов */
+    'CONCERTS_PAGE_TITLE' => 'Концерты',
+    /* Страница медиа */
+    'MEDIA_PAGE_TITLE' => 'Медиа',
+    /* Страница музыки */
+    'MUSIC_PAGE_TITLE' => 'Музыка',
+    /* Страница проектов */
+    'PROJECTS_PAGE_TITLE' => 'Проекты',
+    /* Страница репертуара */
+    'REPERTOIRE_PAGE_TITLE' => 'Репертуар',
+
+    /* Страница контактов */
+    'CONTACTS_PAGE_TITLE' => 'Контакты',
+    'CONTACTS_PAGE_TOUCH_WITH_ME' => 'Связаться со мной',
+    'CONTACTS_PAGE_NOTICE' => 'Если у вас есть деловое предложение или другие вопросы, пожалуйста, заполните следующую форму, чтобы связаться со мной. Спасибо!',
+    'CONTACTS_PAGE_SUCCESS_MESSAGE' => 'Благодарим Вас за обращение к нам. Мы ответим вам как можно скорее.',
+    /* Страница входа */
+    'SIGN_IN_PAGE_TITLE' => 'Вход',
+    'SIGN_IN_PAGE_TEXT' => 'Пожалуйста, заполните следующие поля для входа:',
+    'SIGN_IN_PAGE_RESET_TEXT' => 'Если Вы забыли свой пароль, то Вы можете',
+    'SIGN_IN_PAGE_RESET_LINK' => 'сбросить его',
+
+    /* Страницы административной части сайта */
+    /* Страницы учетной записи пользователя (администратора) */
+    'USER_ADMIN_PAGE_MY_PROFILE' => 'Мои профиль',
+    'USER_ADMIN_PAGE_MY_BIOGRAPHY' => 'Моя биография',
+    'USER_ADMIN_PAGE_UPDATE_DATA' => 'Обновление личных данных',
+    'USER_ADMIN_PAGE_UPDATE_BIOGRAPHY' => 'Обновление биографии',
+    'USER_ADMIN_PAGE_UPDATE_PASSWORD' => 'Смена пароля',
+    /* Кнопки на страницах учетной записи пользователя (администратора) */
+    'USER_ADMIN_PAGE_BUTTON_UPDATE_DATA' => 'Изменить личные данные',
+    'USER_ADMIN_PAGE_BUTTON_UPDATE_BIOGRAPHY' => 'Изменить биографию',
+    'USER_ADMIN_PAGE_BUTTON_UPDATE_PASSWORD' => 'Сменить пароль',
+    /* Сообщения на страницах аучетной записи пользователя (администратора) */
+    'USER_ADMIN_PAGE_MESSAGE_UPDATED_DATA' => 'Вы успешно обновили личные данные.',
+    'USER_ADMIN_PAGE_MESSAGE_UPDATED_BIOGRAPHY' => 'Вы успешно обновили биографию.',
+    'USER_ADMIN_PAGE_MESSAGE_UPDATE_PASSWORD' => 'Вы успешно обновили пароль.',
+    /* Страницы администрирования концертов */
+    'CONCERTS_ADMIN_PAGE_CONCERTS' => 'Концерты',
+    /* Страницы администрирования медиа */
+    'MEDIA_ADMIN_PAGE_MEDIA' => 'Медиа',
+    /* Страницы администрирования музыки */
+    'MUSIC_ADMIN_PAGE_MUSIC' => 'Музыка',
+    /* Страницы администрирования проектов */
+    'PROJECTS_ADMIN_PAGE_PROJECTS' => 'Проекты',
+    /* Страницы администрирования проектов */
+    'REPERTOIRE_ADMIN_PAGE_REPERTOIRE' => 'Репертуар',
+
+    /* Формы */
+    /* ContactForm */
+    'CONTACT_FORM_NAME' => 'Имя',
+    'CONTACT_FORM_EMAIL' => 'Электронная почта',
+    'CONTACT_FORM_SUBJECT' => 'Тема',
+    'CONTACT_FORM_MESSAGE' => 'Сообщение',
+    'CONTACT_FORM_VERIFICATION_CODE' => 'Проверочный код',
+    /* LoginForm */
+    'LOGIN_FORM_USERNAME' => 'Имя пользователя',
+    'LOGIN_FORM_PASSWORD' => 'Пароль',
+    'LOGIN_FORM_REMEMBER_ME' => 'Запомнить меня',
+    /* Сообщения LoginForm */
+    'LOGIN_FORM_MESSAGE_INCORRECT_USERNAME_OR_PASSWORD' => 'Неверное имя пользователя или пароль.',
+
+    /* Модели */
+    /* Lang */
+    'LANG_MODEL_ID' => 'ID',
+    'LANG_MODEL_CREATED_AT' => 'Создан',
+    'LANG_MODEL_UPDATED_AT' => 'Обновлен',
+    'LANG_MODEL_URL' => 'URL',
+    'LANG_MODEL_LOCAL' => 'Локаль',
+    'LANG_MODEL_NAME' => 'Название',
+    'LANG_MODEL_DEFAULT' => 'Язык по умолчанию',
+    /* User */
+    'USER_MODEL_ID' => 'ID',
+    'USER_MODEL_CREATED_AT' => 'Зарегистрирован',
+    'USER_MODEL_UPDATED_AT' => 'Обновлен',
+    'USER_MODEL_USERNAME' => 'Логин',
+    'USER_MODEL_PASSWORD' => 'Пароль',
+    'USER_MODEL_AUTH_KEY' => 'Ключ аутентификации',
+    'USER_MODEL_EMAIL_CONFIRM_TOKEN' => 'Метка подтверждения электронной почты',
+    'USER_MODEL_PASSWORD_HASH' => 'Хэш пароля',
+    'USER_MODEL_PASSWORD_RESET_TOKEN' => 'Метка сброса пароля',
+    'USER_MODEL_FULL_NAME_RU' => 'Фамилия Имя Отчество (на русском)',
+    'USER_MODEL_FULL_NAME_EN' => 'Фамилия Имя Отчество (на английском)',
+    'USER_MODEL_ADDRESS_RU' => 'Адрес (на русском)',
+    'USER_MODEL_ADDRESS_EN' => 'Адрес (на английском)',
+    'USER_MODEL_BIOGRAPHY_RU' => 'Биография (на русском)',
+    'USER_MODEL_BIOGRAPHY_EN' => 'Биография (на английском)',
+    'USER_MODEL_EMAIL' => 'Электронная почта',
+    'USER_MODEL_PHONE' => 'Телефон',
+    'USER_MODEL_YOUTUBE_LINK' => 'Ссылка на YouTube',
+    'USER_MODEL_INSTAGRAM_LINK' => 'Ссылка на Instagram',
+    'USER_MODEL_FACEBOOK_LINK' => 'Ссылка на Facebook',
+    'USER_MODEL_TWITTER_LINK' => 'Ссылка на Twitter',
+    'USER_MODEL_VK_LINK' => 'Ссылка на VK',
+    'USER_MODEL_APPLE_MUSIC_LINK' => 'Ссылка на Apple Music',
+    'USER_MODEL_YANDEX_MUSIC_LINK' => 'Ссылка на Yandex Music',
+    'USER_MODEL_SPOTIFY_LINK' => 'Ссылка на Spotify',
+    /* Сообщения модели User */
+    'USER_MODEL_MESSAGE_USERNAME' => 'Это имя пользователя уже занято.',
+    'USER_MODEL_MESSAGE_UPDATED_YOUR_DETAILS' => 'Вы успешно изменили свои данные.',
+    'USER_MODEL_MESSAGE_UPDATED_YOUR_PASSWORD' => 'Вы успешно изменили пароль.',
+    'USER_MODEL_MESSAGE_FULL_NAME_RU' => 'Ваша фамилия, имя, отчество должны быть на русском языке.',
+    'USER_MODEL_MESSAGE_FULL_NAME_EN' => 'Ваша фамилия, имя, отчество должны быть на английском языке.',
+];

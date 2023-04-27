d8f1618d3bf2d8f25ea8848159179f8054adaa74
LedZeppe1in <tualatin32@mail.ru>
2023-04-27 15:11:53 +0800

    Added VideoSearch model and video controller, and minor fixed repertoire controller

--- modules/admin/controllers/VideoController.php
+++ modules/admin/controllers/VideoController.php
@@ -6,13 +6,13 @@ use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use app\modules\admin\models\Repertoire;
-use app\modules\admin\models\RepertoireSearch;
+use app\modules\admin\models\Video;
+use app\modules\admin\models\VideoSearch;
 
 /**
- * RepertoireController implements the CRUD actions for Repertoire model.
+ * VideoController implements the CRUD actions for Video model.
  */
-class RepertoireController extends Controller
+class VideoController extends Controller
 {
     public $layout = 'admin';
 
@@ -35,13 +35,13 @@ class RepertoireController extends Controller
     }
 
     /**
-     * Lists all Repertoire models.
+     * Lists all Video models.
      *
      * @return string
      */
     public function actionList()
     {
-        $searchModel = new RepertoireSearch();
+        $searchModel = new VideoSearch();
         $dataProvider = $searchModel->search($this->request->queryParams);
 
         return $this->render('list', [
@@ -51,7 +51,7 @@ class RepertoireController extends Controller
     }
 
     /**
-     * Displays a single Repertoire model.
+     * Displays a single Video model.
      *
      * @param $id
      * @return string
@@ -65,19 +65,19 @@ class RepertoireController extends Controller
     }
 
     /**
-     * Creates a new Repertoire model.
+     * Creates a new Video model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      *
      * @return string|\yii\web\Response
      */
     public function actionCreate()
     {
-        $model = new Repertoire();
+        $model = new Video();
 
         if ($this->request->isPost) {
             if ($model->load($this->request->post()) && $model->save()) {
                 Yii::$app->getSession()->setFlash('success',
-                    Yii::t('app', 'REPERTOIRE_ADMIN_PAGE_MESSAGE_CREATE_REPERTOIRE'));
+                    Yii::t('app', 'VIDEO_ADMIN_PAGE_MESSAGE_CREATE_VIDEO'));
 
                 return $this->redirect(['view', 'id' => $model->id]);
             }
@@ -90,7 +90,7 @@ class RepertoireController extends Controller
     }
 
     /**
-     * Updates an existing Repertoire model.
+     * Updates an existing Video model.
      * If update is successful, the browser will be redirected to the 'view' page.
      *
      * @param $id
@@ -103,7 +103,7 @@ class RepertoireController extends Controller
 
         if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
             Yii::$app->getSession()->setFlash('success',
-                Yii::t('app', 'REPERTOIRE_ADMIN_PAGE_MESSAGE_UPDATED_REPERTOIRE'));
+                Yii::t('app', 'VIDEO_ADMIN_PAGE_MESSAGE_UPDATED_VIDEO'));
 
             return $this->redirect(['view', 'id' => $model->id]);
         }
@@ -114,7 +114,7 @@ class RepertoireController extends Controller
     }
 
     /**
-     * Deletes an existing Repertoire model.
+     * Deletes an existing Video model.
      * If deletion is successful, the browser will be redirected to the 'list' page.
      *
      * @param $id
@@ -127,22 +127,22 @@ class RepertoireController extends Controller
     {
         $this->findModel($id)->delete();
         Yii::$app->getSession()->setFlash('success',
-            Yii::t('app', 'REPERTOIRE_ADMIN_PAGE_MESSAGE_DELETED_REPERTOIRE'));
+            Yii::t('app', 'VIDEO_ADMIN_PAGE_MESSAGE_DELETED_VIDEO'));
 
         return $this->redirect(['list']);
     }
 
     /**
-     * Finds the Repertoire model based on its primary key value.
+     * Finds the Video model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      *
      * @param $id
-     * @return Repertoire the loaded model
+     * @return Video the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
     protected function findModel($id)
     {
-        if (($model = Repertoire::findOne(['id' => $id])) !== null)
+        if (($model = Video::findOne(['id' => $id])) !== null)
             return $model;
 
         throw new NotFoundHttpException(Yii::t('app', 'ERROR_MESSAGE_PAGE_NOT_FOUND'));

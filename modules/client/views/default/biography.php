63c01897b780274ca1c319188f995405f6483343
LedZeppe1in <tualatin32@mail.ru>
2023-04-28 17:32:33 +0800

    Updated biography page, and added biography.jpg

--- modules/client/views/default/biography.php
+++ modules/client/views/default/biography.php
@@ -1,12 +1,129 @@
 <?php
 
 /* @var $this yii\web\View */
+/* @var $user app\modules\admin\models\User */
+
+use yii\bootstrap5\Html;
+use yii\helpers\Url;
 
 $this->title = Yii::t('app', 'BIOGRAPHY_PAGE_TITLE');
 ?>
 
-<section class="section section-lg bg-default">
-    <div class="container">
-        <h3 class="custom-title"><?= Yii::t('app', 'BIOGRAPHY_PAGE_TITLE') ?></h3>
+<a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars" style="color:"></i></a>
+<nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle">
+            <i class="fa fa-times"></i>
+        </a>
+        <li class="sidebar-brand">
+            <a href="<?= Url::to(['default/index']) ?>"><?= Yii::t('app', 'NAV_HOME') ?></a>
+        </li>
+        <li class="active">
+            <a href="<?= Url::to(['default/biography']) ?>">
+                <?= Yii::t('app', 'NAV_BIOGRAPHY') ?>
+            </a>
+        </li>
+        <li class>
+            <a href="<?= Url::to(['default/concerts']) ?>">
+                <?= Yii::t('app', 'NAV_CONCERTS') ?>
+            </a>
+        </li>
+        <li class>
+            <a href="<?= Url::to(['default/media']) ?>">
+                <?= Yii::t('app', 'NAV_MEDIA') ?>
+            </a>
+        </li>
+        <li class>
+            <a href="<?= Url::to(['default/music']) ?>">
+                <?= Yii::t('app', 'NAV_MUSIC') ?>
+            </a>
+        </li>
+        <li class>
+            <a href="<?= Url::to(['default/projects']) ?>">
+                <?= Yii::t('app', 'NAV_PROJECTS') ?>
+            </a>
+        </li>
+        <li class>
+            <a href="<?= Url::to(['default/repertoire']) ?>">
+                <?= Yii::t('app', 'NAV_REPERTOIRE') ?>
+            </a>
+        </li>
+        <li class>
+            <a href="<?= Url::to(['default/contacts']) ?>">
+                <?= Yii::t('app', 'NAV_CONTACTS') ?>
+            </a>
+        </li>
+        <?php if (Yii::$app->user->isGuest)
+            echo '<li>' . Html::a(Yii::t('app', 'NAV_SIGN_IN'), ['default/sing-in']) . '</li>';
+        else
+            echo '<li>' . Html::a(Yii::t('app', 'NAV_ADMINISTRATION'), ['/admin/user/profile']) .
+                '</li><li>' . Html::a(Yii::t('app', 'NAV_SIGN_OUT'), ['default/sing-out']) . '</li>';
+        ?>
+        <li>
+            <?= Html::a(Yii::$app->language == 'ru-RU' ?
+                "<figure class='icon-lang icon-en'></figure> English" :
+                "<figure class='icon-lang icon-ru'></figure> Русский",
+                Yii::$app->language == 'ru-RU' ? '/en' . Yii::$app->getRequest()->getLangUrl() : '/ru' .
+                    Yii::$app->getRequest()->getLangUrl(), ['class' => 'rd-nav-link']) ?>
+        </li>
+    </ul>
+</nav>
+
+<div class="container cover-container" style="z-index:;">
+    <div class="cover" style="background-image: url(/images/biography.jpg); background-position:% 50%;"></div>
+    <div class="title-block">
+        <div class="row title-row">
+            <div class="col-sm-12 title-col">
+                <h1 class="header"><?= Yii::t('app', 'BIOGRAPHY_PAGE_TITLE') ?><br></h1>
+                <div class="angle">
+                    <a href="#body" class="btn-scroll">
+                        <i class="fa fa-angle-down animated" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
-</section>
+</div>
+
+<div class="container biography content-box" id="body">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 content">
+            <?php
+                if (Yii::$app->language == 'ru-RU')
+                    echo $user->biography_ru;
+                else
+                    echo $user->biography_en;
+            ?>
+        </div>
+    </div>
+
+    <footer class="footer" style="position: bottom:">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 text-left">&copy; <?= date('Y') . ' ' .
+                    Yii::t('app', 'FOOTER_LOGO') ?></div>
+                <div class="col-sm-4 text-center">
+                    <?php if ($user->youtube_link): ?>
+                        <a href="<?= $user->youtube_link ?>"><p class="fa-brands fa-youtube fa-lg footer-icon"></p></a>
+                    <?php endif; ?>
+                    <?php if ($user->instagram_link): ?>
+                        <a href="<?= $user->instagram_link ?>"><p class="fa-brands fa-instagram fa-lg footer-icon"></p></a>
+                    <?php endif; ?>
+                    <?php if ($user->facebook_link): ?>
+                        <a href="<?= $user->facebook_link ?>"><p class="fa-brands fa-facebook fa-lg footer-icon"></p></a>
+                    <?php endif; ?>
+                    <?php if ($user->twitter_link): ?>
+                        <a href="<?= $user->twitter_link ?>"><p class="fa-brands fa-twitter fa-lg footer-icon"></p></a>
+                    <?php endif; ?>
+                    <?php if ($user->vk_link): ?>
+                        <a href="<?= $user->vk_link ?>"><p class="fa-brands fa-vk fa-lg footer-icon"></p></a>
+                    <?php endif; ?>
+                </div>
+                <div class="col-sm-4 text-right"><?= Yii::t('app', 'FOOTER_POWERED_BY') .
+                    ' <a href="https://github.com/LedZeppe1in">' . Yii::t('app', 'FOOTER_DEVELOPER') .
+                    '</a>' ?>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>

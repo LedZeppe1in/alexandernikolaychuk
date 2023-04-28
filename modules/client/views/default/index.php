b4bc43a7fb79cca1fac81f289533dd9f52440e6f
LedZeppe1in <tualatin32@mail.ru>
2023-04-28 16:35:33 +0800

    Added new font and logo for client site part

--- modules/client/views/default/index.php
+++ modules/client/views/default/index.php
@@ -3,6 +3,8 @@
 /* @var $this yii\web\View */
 /* @var $user app\modules\admin\models\User */
 
+use yii\bootstrap5\Html;
+
 $this->title = Yii::$app->language == 'ru-RU' ? $user->full_name_ru : $user->full_name_en;
 ?>
 
@@ -13,8 +15,12 @@ $this->title = Yii::$app->language == 'ru-RU' ? $user->full_name_ru : $user->ful
 <div class="container home-container">
     <div class="row home-row">
         <div class="col-md-12 home-col">
-            <h1 class="home-heading"><?= Yii::t('app', 'HOME_PAGE_TITLE') ?></h1><br>
-            <p class="home-paragraph"><?= Yii::t('app', 'HOME_PAGE_TEXT') ?></p>
+            <?php
+                if (Yii::$app->language == 'ru-RU')
+                    echo Html::img('@web/images/client-logo-ru.png', ['class'=>'client-logo']);
+                else
+                    echo Html::img('@web/images/client-logo-en.png', ['class'=>'client-logo']);
+            ?>
         </div>
     </div>
 </div>
